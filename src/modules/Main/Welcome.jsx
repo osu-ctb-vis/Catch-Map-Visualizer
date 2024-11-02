ebc289ce33d63a5cc552ee043b7b378a4e797889
solstice23 <solstice233@gmail.com>
2024-11-02 15:05:49 -0400

    Update welcome image

--- src/modules/Main/Welcome.jsx
+++ src/modules/Main/Welcome.jsx
@@ -2,7 +2,7 @@ import { useState, useContext, useEffect, useRef } from 'react';
 import { MapPackContext } from '../../contexts/MapPackContext';
 import { BeatmapsContext } from '../../contexts/BeatmapsContext';
 import clsx from 'clsx';
-import WelcomePng from '../../assets/welcome.png';
+import WelcomeSvg from '../../assets/welcome.svg';
 import './Welcome.scss';
 
 export function Welcome() {
@@ -10,7 +10,7 @@ export function Welcome() {
 
 	if (!mapPack) return (
 		<div className="welcome">
-			<img src={WelcomePng} className="welcome-image" onDragStart={e => e.preventDefault()} />
+			<img src={WelcomeSvg} className="welcome-image" onDragStart={e => e.preventDefault()} />
 			<div className="welcome-text">Catch Map Visualizer</div>
 			<div className="welcome-instructions">Select or drag a beatmap to start</div>
 			<a href="https://github.com/osu-ctb-vis/Catch-Map-Visualizer" target="_blank" className="welcome-link">GitHub</a>
