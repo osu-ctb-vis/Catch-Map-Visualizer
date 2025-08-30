8bc47c8b8a968f05eae4e21221cef9a4fb6f554d
solstice23 <solstice233@gmail.com>
2025-08-29 23:00:22 -0400

    Add button for loading example beatmap

--- src/modules/Main/Welcome.jsx
+++ src/modules/Main/Welcome.jsx
@@ -7,12 +7,18 @@ import './Welcome.scss';
 
 export function Welcome() {
 	const { mapPack } = useContext(MapPackContext);
+	const [loadExampleClicked, setLoadExampleClicked] = useState(false);
+
+	const loadExampleBeatmap = () => {
+		document.dispatchEvent(new CustomEvent("load-beatmap-from-url", { detail: "https://osu.ppy.sh/beatmapsets/1667811#fruits/3405617" }));
+		setLoadExampleClicked(true);
+	};
 
 	if (!mapPack) return (
 		<div className="welcome">
 			<img src={WelcomeSvg} className="welcome-image" onDragStart={e => e.preventDefault()} />
 			<div className="welcome-text">Catch Map Visualizer</div>
-			<div className="welcome-instructions">Select or drag a beatmap to start</div>
+			<div className="welcome-instructions">Select or drag a beatmap to start<br/> or <div className={clsx("welcome-load-example", { disabled: loadExampleClicked })} onClick={loadExampleBeatmap}>load an example beatmap</div></div>
 			<a href="https://github.com/osu-ctb-vis/Catch-Map-Visualizer" target="_blank" className="welcome-link">GitHub</a>
 		</div>
 	);
